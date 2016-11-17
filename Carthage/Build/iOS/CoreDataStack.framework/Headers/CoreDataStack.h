7665e9ab1153e84f278770ee5bad88aad9843934
Justin Makaila <justinmakaila@gmail.com>
2016-11-17 16:12:12 -0500

    Initial commit

--- Carthage/Build/iOS/CoreDataStack.framework/Headers/CoreDataStack.h
+++
@@ -1,8 +0,0 @@
-@import CoreData;
-
-//! Project version number for CoreDataStack.
-FOUNDATION_EXPORT double CoreDataStackVersionNumber;
-
-//! Project version string for CoreDataStack.
-FOUNDATION_EXPORT const unsigned char CoreDataStackVersionString[];
-
