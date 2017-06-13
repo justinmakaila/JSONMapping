547ba30eccf56b9a01841b444fffc06bae71cc79
Justin Makaila <justinmakaila@gmail.com>
2017-06-13 17:53:31 -0400

    Checked out Carthage directory

--- Carthage/Checkouts/RemoteMapping/RemoteMapping/RemoteEntityType.swift
+++
@@ -1,7 +0,0 @@
-/// Represents an entity and it's primary keys
-public protocol RemoteEntityType {
-    /// The remote primary key name.
-    var remotePrimaryKeyName: String { get }
-    /// The local primary key name.
-    var localPrimaryKeyName: String { get }
-}
